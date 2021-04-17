ed6613a84d8ad81b3de5f2aaa9454d098c021106
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2021-04-17 13:40:07 +0300

    Added JS for first unit

--- app.js
+++ app.js
@@ -1,3 +1,53 @@
+// document - globāls objekts, kas pieejams Javascript kodā, apzīmē mūsu mājaslapas dokumentu
+
+// Pievienojam funkciju, kas izpildas, kad visa lapa ir ielādējusies
+// Izmanto, lai pārliecinātos, ka visi elementi ir pieejami
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+    // setTimeout(funkcija, laiks) --> funkcija, kas izpilda citu funkciju pēc konkrēta laika sprīža
+
+    // Lai popup elements parādās pēc 5s
+    setTimeout(show, 5000);
+
+    // addEventListener(notikums, funkcija) --> izpilda funkciju, ja notiek notikums 
+    // (piemēram, click, hover, mousedown) 
+
+    // Pazūd, ja aizveram
+    document
+        .querySelector('.popup button') // Izvēlamies elementu
+        .addEventListener('click', hide); // Pieliekam funkciju, kas izpildīsies ja uz elementa noklikšķina
+
+});
+
+// Funkcija, lai parādītu popup elementu
+function show() {
+    // Izvēlamies konkrēto elementu
+    var elementToShow = document.querySelector('.popup');
+
+    // Nolasām sīkdatnes vērtību
+    var closedPopup = readCookie('closed-popup');
+
+    // ja ir vērtība un tā nav yes, tad mēs parādām popup elementu
+    if (closedPopup != 'yes') {
+        elementToShow.style.visibility = 'visible';
+        elementToShow.style.opacity = 1;
+    }
+}
+
+// Funkcija, lai paslēptu popup elementu
+function hide() {
+    // Izvēlamies konkrēto elementu
+    var elementToHide = document.querySelector('.popup');
+
+    // Uzstādam CSS atribūtus - visibility un opacity
+    elementToHide.style.visibility = 'hidden';
+    elementToHide.style.opacity = 0;
+
+    // Izveidojam sīkdatni, lai norādītu, ka aizvērām
+    createCookie('closed-popup', 'yes', 5);
+}
+
+
 function createCookie(name, value, days) {
     var expires;
 
